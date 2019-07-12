f593a639598a4250e124dc15f7d736a8213f01e7
philong196 <52817627+philong196@users.noreply.github.com>
2019-07-12 15:35:43 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# B-o-Ph-p-Lu-t-TP
-Tra Điểm Thi 
+# Chatbot Tra cứu điểm thi THPT Quốc Gia 2018
+Điểm thi được cập nhật từ http://diemthi.vnexpress.net
+
+[Demo](https://www.facebook.com/so3botrach/videos/1383843391670405/)
+
+# Cài đặt
+- Tải source code về
+- Upload source code lên host
+- Vào [Chatfuel](https://chatfuel.com/) tạo tài khoản và liên kết với Fanpage cần tạo chatbot
+- Sửa mục `Welcome message`
+<img src="http://i.imgur.com/upr47Af.png" />
+- Sửa mục `Default Answer`
+<img src="https://i.imgur.com/fTD6TVs.png" />
+- Test Chatbot :D
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php 
+
+include ('./lib/Document.php');
+include ('./lib/Element.php');
+include ('./lib/Encoder.php');
+include ('./lib/Errors.php');
+include ('./lib/Query.php');
+include ('./lib/Chatfuel.php');
+
+$chatfuel = new Chatfuel(TRUE);
+
+$html = getUser($_GET["sbd"]);
+
+if($html) {
+	$results = [];
+	$getMon = (new Document($html))->find('.width_monhoc');
+	$getDiem = (new Document($html))->find('.width_sbd');
+	$getDiem = array_slice($getDiem, 3);
+
+	foreach ($getMon as $key => $value) {
+		$results[$value->text()] = $getDiem[$key+1]->text();
+	}
+
+	$getSDB = (new Document($html))->find('.width_sbd a')[0]->text();
+	$ketqua = "Thí sinh SDB: $getSDB \n";
+
+	foreach ($results as $mon => $diem) {
+		if(!$diem) {
+			continue;
+		}
+		$ketqua .= "💯 $mon: $diem \n"; 
+	}
+
+	$chatfuel->sendText($ketqua);
+
+} else {
+	$chatfuel->sendText("Không tìm thấy kết quả, vui lòng nhập đúng số báo danh 🙏");
+}
+
+function getUser($q) {
+	$mavung = substr($q, 0, 2);
+
+	$ch = curl_init();
+	
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201');
+	curl_setopt($ch, CURLOPT_URL, 'http://tradiemthi.plo.vn/diem-thi-thpt/'. $q .'&college=' . $mavung . '&area=2');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+	curl_exec($ch);
+
+	$resurt = curl_exec($ch);
+
+	curl_close($ch);
+
+	$data = json_decode($resurt, true)["data"];
+
+	if($data == 'Không tìm thấy kết quả') return false;
+
+	return $data;
+}
+
+function getDiem($url) {
+	$ch = curl_init();
+	
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201');
+	curl_setopt($ch, CURLOPT_URL, 'http://tradiemthi.plo.vn/'.$url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_exec($ch);
+
+	$resurt = curl_exec($ch);
+
+	curl_close($ch);
+
+	return $resurt;
+}
